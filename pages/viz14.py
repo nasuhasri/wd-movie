208e2fe68ab3ccb0c8430d1ae5b5fb0182fdf101
Nasuha Asri <nasuhasri00@gmail.com>
2024-07-09 23:27:52 +0800

    Update viz14.py

--- pages/viz14.py
+++ pages/viz14.py
@@ -1,20 +1,27 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.write("Distribution of movie and tv series by year")
 
-df = pd.DataFrame(pd.read_pickle(r'dataset\data-viz14.pkl'))
+file_path = os.path.join('dataset', 'data-viz14.pkl')
 
-# get movies data
-df_movies = df[df['titleType'] == 'movie']
-# get series data
-df_tvSeries = df[df['titleType'] == 'tvSeries']
+if os.path.exists(file_path):
+    
+    df = pd.DataFrame(pd.read_pickle(r'dataset\data-viz14.pkl'))
 
-chart_data = pd.DataFrame({
-    "Year": df['startYear'],
-    "Movies": df_movies['primaryTitle'],
-    "TV Series": df_tvSeries['primaryTitle']
-})
+    # get movies data
+    df_movies = df[df['titleType'] == 'movie']
+    # get series data
+    df_tvSeries = df[df['titleType'] == 'tvSeries']
 
-# draw chart
-st.bar_chart(chart_data, x="Year", y=["Movies", "TV Series"], x_label='Released Year', y_label='Number of Movies and Series Titles')
+    chart_data = pd.DataFrame({
+        "Year": df['startYear'],
+        "Movies": df_movies['primaryTitle'],
+        "TV Series": df_tvSeries['primaryTitle']
+    })
+
+    # draw chart
+    st.bar_chart(chart_data, x="Year", y=["Movies", "TV Series"], x_label='Released Year', y_label='Number of Movies and Series Titles')
+else:
+    st.error(f"File not found: {file_path}")
